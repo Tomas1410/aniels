ad8bd2643805e01c6aea3abdbd54dbd2483990d2
Tomaszex <t.uryga@student.po.edu.pl>
2021-05-31 17:06:29 +0200

    deploy v2

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,13 @@ const initialState = {};
 
 const middleware = [thunk];
 
-
 const store = createStore(
     rootReducer,
     initialState,
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(...middleware)
+        //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
     )
 );
 
